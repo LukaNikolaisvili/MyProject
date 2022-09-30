deed63bdbbe1b00ded199b1d5fa662ca078e21d4
LukaNikolaisvili <jokerpoker1213@gmail.com>
2022-09-30 01:02:32 -0400

    added more code into it

--- project.cs
+++ project.cs
@@ -15,11 +15,10 @@ public class Puzzle
 private Square[ , ] grid;
 private int N;
 // Create an NxN crossword grid of WHITE squares (4 marks)
-
 public Puzzle (int N) { 
 
     grid.Equals(N);
-    "---hello"
+
  }
 // Randomly initialize a crossword grid with M black squares (5 marks)
 public void Initialize (int M) { 
@@ -38,7 +37,57 @@ public void PrintGrid ( ) {  }
 
 
 
-hello, how are you...
+// hello, how are you...
 
  }
 
+
+public class MyString
+{
+ private class Node {
+ public char item;
+ public Node next;
+ // Constructor (2 marks)
+ public Node (  ) { 
+
+
+
+  }
+}
+private Node front; // Reference to the first (header) node
+private int length; // Number of characters in MyString
+// Initialize with a header node an instance of MyString to the given character array A (4 marks)
+public MyString (char[ ] A) {
+
+
+
+}
+// Using a stack, reverse this instance of MyString (6 marks)
+public void Reverse ( ) { 
+    Stack<char> S; 
+
+for(int i = )
+}
+// Return the index of the first occurrence of c in this instance; otherwise -1 (4 marks)
+public int IndexOf(char c) {
+
+    return 1;
+ }
+// Remove all occurrences of c from this instance (4 marks)
+public void Remove (char c) {
+
+}
+// Return true if obj is both of type MyString and the same as this instance;
+// otherwise false (6 marks)
+public override bool Equals (object obj) {
+    return true;
+
+
+}
+
+
+// Print out this instance of MyString (3 marks)
+public void Print( ) {
+
+ }
+}
