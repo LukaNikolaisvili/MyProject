934b973c7315dda6783c8a2401503b51a285d903
LukaNikolaisvili <jokerpoker1213@gmail.com>
2022-10-16 22:46:19 -0400

    added changes on the Puzzle and Square class

--- PuzzleAndSquare.cs
+++ PuzzleAndSquare.cs
@@ -1,15 +1,18 @@
-﻿public class Square
+﻿// Group Members: Farzad Imran (0729901), Luka Nikolaisvili (0674677), Abdulrahman Saeed (0706145)  | Assignment 1 Part A
+public class Square
 {
     public enum TColor { WHITE, BLACK };
     private Square[,] grid;
     private int N;
     public TColor Color { set; get; } // Either WHITE or BLACK
     public int Clue { set; get; } // Either a clue number or -1 (Note: A BLACK square is always -1)
+    public string Value { set; get; } // Gives each Square a value to print, X, Blank or a Number.
+    
     public static void Main(string[] args)
     {
         int size, black = 0;
-        string[,] arr = { { "   " } };
-        Square square = new Square(arr);
+
+        Square square = new Square();
 
         // Enter and validate the grid size (positive interger)
         do
@@ -19,53 +22,68 @@
         } while (size < 0);
         square.N = size;
         square.Puzzle(size);
-        // Enter and validate the number of black squares (positive interger)
+        // Enter and validate the number of black squares (positive integer, Cannot be larger than the grid dimenstion)
         do
         {
             Console.Write("Enter the number of black squares (positive value) → ");
             black = Convert.ToInt32(Console.ReadLine());
         } while (black < 0 || black > (size * size));
-        // square.Initialize(black);
+        square.Initialize(black);
+        square.Number();
         square.PrintGrid();
+        square.PrintClues();
+        Console.WriteLine("Is the grid symmetrical? {0}", square.Symmetric());
+        Console.ReadLine();
 
     }
     // Initialize a square to WHITE and its clue number to -1 (2 marks)
-    public Square(string[,] grid)
+    public Square()
     {
-       
-        
-        for (int row = 0; row < N; row++)
+
+        this.Color = TColor.WHITE;
+        this.Clue = -1;
+        this.Value = "   ";
+    }
+
+
+    // Create an NxN crossword grid of WHITE squares (4 marks)
+    public void Puzzle(int N)
+    {
+        this.N = N;
+
+        grid = new Square[N+1, N+1];
+
+        for (int i = 0; i < N; i++)
         {
-            for (int col = 0; col < N; col++)
+            for (int j = 0; j < N; j++)
             {
-                this.Color = TColor.WHITE;
-                this.Clue = -1;
-                grid[row, col] = "   ";
+                grid[i, j] = new Square();
             }
         }
-    }
-    // Create an NxN crossword grid of WHITE squares (4 marks)
-    public object Puzzle(int N)
-    {
-        Square[,] grid = new Square[N, N];
-        return grid;
+        for (int i = 0; i <= N; i++)
+        {
+            grid[i, N] = new Square();
+        }
     }
     // Randomly initialize a crossword grid with M black squares (5 marks)
     public void Initialize(int M)
     {
         int BCount = 0;
         Random r = new Random();
-        do
+        if (M != 0)
         {
-            int rX = r.Next(0, M);
-            int rY = r.Next(0, M);
-            if (grid[rX, rY].Color == TColor.WHITE)
+            do
             {
-                this.Color = TColor.BLACK;
-                grid[rX, rY] = " X ";
-                BCount++;
-            }
-        } while (BCount != M);
+                int rX = r.Next(0, N);
+                int rY = r.Next(0, N);
+                if (grid[rX, rY].Color.Equals(TColor.WHITE))
+                {
+                    grid[rX, rY].Color = TColor.BLACK;
+                    grid[rX, rY].Value = " X ";
+                    BCount++;
+                }
+            } while (BCount != M);
+        }
     }
     // Number the crossword grid (6 marks)
     public void Number()
@@ -77,19 +95,19 @@
             {
                 if (grid[row, col].Color != TColor.BLACK)
                 {
-                    if (grid[row, col+1].Color != TColor.BLACK || grid[row + 1, col].Color != TColor.BLACK)
-                    {
+
                         Clue = count;
                         if (count >= 10)
                         {
-                            grid[row, col] = " " + count;
+                            grid[row, col].Clue = count;
+                            grid[row, col].Value = " " + count;
                         }
                         else
                         {
-                            grid[row, col] = " " + count + " ";
+                            grid[row, col].Clue = count;
+                            grid[row, col].Value = " " + count + " ";
                         }
                         count++;
-                    }
                 }
             }
         }
@@ -97,52 +115,53 @@
     // Print out the numbers for the Across and Down clues (in order) (4 marks)
     public void PrintClues()
     {
-         Console.WriteLine("Across");
+        Console.WriteLine("Across");
         for (int row = 0; row < N; row++)
         {
             for (int col = 0; col < N; col++)
             {
                 if (grid[row, col].Clue != -1)
                 {
-                    if (grid[row, col + 1].Color != TColor.BLACK)
+                    if (grid[row, col].Color != TColor.BLACK)
                     {
-                        Console.Write(grid[row, col].Clue);
+                        Console.Write(grid[row, col].Value + " ");
                     }
                 }
             }
         }
+        Console.WriteLine();
         Console.WriteLine("Down");
         for (int row = 0; row < N; row++)
         {
             for (int col = 0; col < N; col++)
             {
-                if (grid[row, col].Clue != -1)
+                if (grid[col, row].Clue != -1)
                 {
-                    if (grid[row + 1, col].Color != TColor.BLACK)
+                    if (grid[row, col].Color != TColor.BLACK)
                     {
-                        Console.Write(grid[row, col].Clue);
+                        Console.Write(grid[row, col].Value + " ");
                     }
                 }
             }
         }
+        Console.WriteLine();
     }
 
     // Print out the crossword grid including the BLACK squares and clue numbers (5 marks)
     public void PrintGrid()
     {
         const string line = "-";
-
-        string[,] arr = {{" 1 "," 2 "," 2 "," 4 "," 10"," 12"," 4 "," 11"," 23"," 9 "," 1 "," 2 "," 5 "," 4 "," 3 "," 5 "," 4 "," 7 "," 8 "," X "," 2 "," 2 "," 5 "," 4 "," 3 "," 5 "," 4 "," 7 "," 8 "," 9 "},{" 10"," 11"," 7 "," 5 "," 4 "," 8 "," 7 "," 20"," 21"," 9 "," 1 "," 2 "," 5 "," 4 "," 3 "," 5 "," 4 "," 7 "," 8 "," 9 "," 1 "," 2 "," 5 "," 4 "," 3 "," 5 "," 4 "," 7 "," 8 "," 9 "},{" 10"," 11"," 7 "," 5 "," 4 "," 8 "," 7 "," 20"," 21"," 9 "," 1 "," 2 "," 5 "," 4 "," 3 "," 5 "," 4 "," 7 "," 8 "," 9 "," 1 "," 2 "," 5 "," 4 "," 3 "," 5 "," 4 "," 7 "," 8 "," 9 "}};
-
         for (int row = 0; row < N; row++)
         {
             for (int x = 0; x < (N * 4) + 1; x++)
                 Console.Write(line);
+
+
             Console.WriteLine();
             for (int col = 0; col < N; col++)
             {
                 Console.Write("|");
-                 Console.Write("{0}", arr[row, col]);
+                Console.Write("{0}", grid[row, col].Value);
             }
             Console.Write("|");
             Console.WriteLine();
@@ -150,18 +169,12 @@
         for (int x = 0; x < (N * 4) + 1; x++)
             Console.Write(line);
         Console.WriteLine();
-        Console.ReadLine();
     }
 
-    public static implicit operator Square(string v)
-    {
-        throw new NotImplementedException();
-    }
     // Return true if the grid is  (à la New York Times); false otherwise (4 marks)
     public bool Symmetric()
     {
-       
-    
+
         bool Result = false;
         for (int row = 0; row < N; row++)
         {
@@ -174,12 +187,14 @@
                         Result = true;
                     }
                     else
+                    {
                         Result = false;
+                        return Result;
+                    }
                 }
             }
         }
         return Result;
-        
 
-    } 
+    }
 }
